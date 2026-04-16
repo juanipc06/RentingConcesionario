c5eec2181974abc870b7ca0c14654e87c198ff37
jpercamb <jpercamb@g.educaand.es>
2026-04-16 09:36:36 +0200

    He cambiado varios metodos de la clase Coche

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola, soy Juani");
         try {
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 
