a047cee5a1c4c5f4bf84c053645b5ec216c96241
Bayarri24 <luisbayarri12@gmail.com>
2026-04-16 09:33:28 +0200

    Modificado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,39 @@
 import ui.VentanaPrincipal;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // Cross-platform para que respete los colores personalizados
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 
-            // Forzar colores oscuros en todos los JComboBox
-            Color fondo   = new Color(38, 38, 48);
-            Color texto   = new Color(240, 240, 245);
-            Color selec   = new Color(220, 38, 38);
-            Color borde   = new Color(50, 50, 65);
+            Color fondo = new Color(38, 38, 48);
+            Color texto = new Color(240, 240, 245);
+            Color selec = new Color(220, 38, 38);
+            Color borde = new Color(50, 50, 65);
 
-            UIManager.put("ComboBox.background",              fondo);
-            UIManager.put("ComboBox.foreground",              texto);
-            UIManager.put("ComboBox.selectionBackground",     selec);
-            UIManager.put("ComboBox.selectionForeground",     texto);
-            UIManager.put("ComboBox.buttonBackground",        fondo);
-            UIManager.put("ComboBox.disabledBackground",      fondo);
+            UIManager.put("ComboBox.background", fondo);
+            UIManager.put("ComboBox.foreground", texto);
+            UIManager.put("ComboBox.selectionBackground", selec);
+            UIManager.put("ComboBox.selectionForeground", texto);
+            UIManager.put("ComboBox.buttonBackground", fondo);
+            UIManager.put("ComboBox.disabledBackground", fondo);
 
-            UIManager.put("ComboBoxUI",                       "javax.swing.plaf.basic.BasicComboBoxUI");
+            UIManager.put("ComboBoxUI", "javax.swing.plaf.basic.BasicComboBoxUI");
 
-            UIManager.put("List.background",                  fondo);
-            UIManager.put("List.foreground",                  texto);
-            UIManager.put("List.selectionBackground",         selec);
-            UIManager.put("List.selectionForeground",         texto);
+            UIManager.put("List.background", fondo);
+            UIManager.put("List.foreground", texto);
+            UIManager.put("List.selectionBackground", selec);
+            UIManager.put("List.selectionForeground", texto);
 
-            UIManager.put("TextField.background",             fondo);
-            UIManager.put("TextField.foreground",             texto);
-            UIManager.put("TextField.caretForeground",        texto);
+            UIManager.put("TextField.background", fondo);
+            UIManager.put("TextField.foreground", texto);
+            UIManager.put("TextField.caretForeground", texto);
 
-            UIManager.put("Panel.background",                 new Color(22, 22, 28));
-            UIManager.put("OptionPane.background",            new Color(28, 28, 35));
-            UIManager.put("OptionPane.messageForeground",     texto);
+            UIManager.put("Panel.background", new Color(22, 22, 28));
+            UIManager.put("OptionPane.background", new Color(28, 28, 35));
+            UIManager.put("OptionPane.messageForeground", texto);
 
         } catch (Exception e) {
             e.printStackTrace();
